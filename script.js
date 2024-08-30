161be04a725c67b652de72b87f1a104217335e32
Teekay <tkmaile96@gmail.com>
2024-08-30 04:30:28 +0200

    Validated my Regular expression

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ function validateSyntax() {
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
-        //Regular Expression to ensure input starts wit 'pet_'
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+        //Regular Expression to ensure input starts wit 'pet_' followed by alphanumeric characters
+        let expression = new RegExp('^pet\\d{4}[A-Za-z]+$');
+
+        //Validate Input agaisnt my Regular Expression
+        if (expression.test(input)) {
+            result ="Valid Syntax "
+        }
 
             document.getElementById('result').innerText = result;
 }
+
 
 
