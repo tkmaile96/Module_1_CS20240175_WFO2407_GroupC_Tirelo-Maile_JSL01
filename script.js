2aae8459b359b608b85f1c2078c78d48baec98f3
Teekay <tkmaile96@gmail.com>
2024-08-30 04:46:19 +0200

    validated syntax with the enter button

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         //Regular Expression to ensure input starts wit 'pet_' followed by alphanumeric characters
-        let expression = new RegExp('^pet\\d{4}[A-Za-z]+$');
+        const expression = new RegExp('^pet_\\d{4}[A-Za-z]+$');
 
         //Validate Input agaisnt my Regular Expression
         if (expression.test(input)) {
@@ -20,8 +20,15 @@ function validateSyntax() {
 
 //Fuction to clear the Input
 function clearInput() {
-    document.getElementById('result').innerText= ''; //set 
+    document.getElementById('result').innerText= ''; //set to an empty string to clear the input after each result    
 }
+
+//An Event Listener for Clearing the Pet Input
+document.getElementById('petInput').addEventListener('keypress', function(event){
+    if (event.key === 'Enter') {
+        validateSyntax();
+    }
+})
 
 
 
