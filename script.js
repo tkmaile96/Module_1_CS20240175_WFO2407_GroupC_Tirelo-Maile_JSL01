070b6e39f06f89f02cbde910b72a4f036e36ebdd
Teekay <tkmaile96@gmail.com>
2024-08-30 04:24:46 +0200

    added a regular expression for  naming conversion

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
+
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
+        //Regular Expression to ensure input starts wit 'pet_'
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
             document.getElementById('result').innerText = result;
